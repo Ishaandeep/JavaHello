040325580ec72eccd66d7a3d9cdb9682f45fa8b8
Ishaan <ishaandeep5@gmail.com>
2021-01-11 19:40:03 +0530

    Java first commit

---
+++ Hello.java
@@ -0,0 +1,17 @@
+/**
+ * Hello
+ */
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+    int a =10,b=5, c=20;
+    //System.out.println(a>b || ++a<c);
+    System.out.println(a);
+    if(++a<c){
+        System.out.println(a);
+    }
+    
+
+    }
+}
